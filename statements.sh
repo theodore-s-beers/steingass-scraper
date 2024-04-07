9c2ed62e175b8fc1e51b22b191bea63fd35c1646
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-04-07 10:43:46 +0200

    Update statements

--- statements.sh
+++ statements.sh
@@ -2,19 +2,19 @@
 
 set -Eeuo pipefail
 
-npx wrangler d1 execute steingass-test-13 --remote --command \
+npx wrangler d1 execute steingass-test-14 --remote --command \
 	"CREATE VIRTUAL TABLE ft_all USING fts5(headword_full, definitions, content='entries', content_rowid='id')" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"INSERT INTO ft_all (headword_full, definitions) SELECT headword_full, definitions FROM entries" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"CREATE VIRTUAL TABLE ft_def USING fts5(definitions, content='entries', content_rowid='id')" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"INSERT INTO ft_def (definitions) SELECT definitions FROM entries" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"CREATE VIRTUAL TABLE ft_hw USING fts5(headword_full, content='entries', content_rowid='id')" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"INSERT INTO ft_hw (headword_full) SELECT headword_full FROM entries" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"CREATE VIRTUAL TABLE ft_per USING fts5(headword_persian, content='entries', content_rowid='id')" &&
-	npx wrangler d1 execute steingass-test-13 --remote --command \
+	npx wrangler d1 execute steingass-test-14 --remote --command \
 		"INSERT INTO ft_per (headword_persian) SELECT headword_persian FROM entries"
